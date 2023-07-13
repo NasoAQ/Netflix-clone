91c6f76d49b80621833104564c5bee2fefa149dd
naso <naso@epicode.it>
2023-07-13 13:18:38 +0200

    Terzo slider

---
+++ main.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   if (document.scrollTop > 50) {
+//     document.getElementsByClassName("mySlider-1").className = "test";
+//   } else {
+//     document.getElementsByClassName("mySlider-1").className = "";
+//   }
+// }
+
+
+let Yo = document.getElementById("provo");
+console.log(Yo);
+
+
+function myFunction() {
+    if (document.scrollingElement > 20) {
+        Yo.className = "test";
+        
+    }
+    
+}
