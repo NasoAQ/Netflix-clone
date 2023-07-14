21f770c3754f2e9fce9e2a4db2c51c7b5eae2094
naso <naso@epicode.it>
2023-07-14 16:29:00 +0200

    Prova ok js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
 // function myFunction() {
 //   if (document.scrollTop > 50) {
@@ -9,13 +9,32 @@ window.onscroll = function() {myFunction()};
 // }
 
 
-let Yo = document.getElementById("provo");
-console.log(Yo);
+// let Yo = document.getElementById("provo");
+// console.log(Yo);
 
 
-function myFunction() {
-    if (document.scrollingElement > 50) {
-        Yo.className = "test";
-    }
+// function myFunction() {
+//     if (document.scrollingElement > 50) {
+//         Yo.className = "test";
+//     }
     
-}
+// }
+
+document.addEventListener('DOMContentLoaded', function() {
+    let main = document.querySelector('body');
+    let secondSlider = document.querySelector('.second-slider');
+    let scrollThreshold = main.offsetHeight * 0.2;
+  
+    function handleScroll() {
+      let scrollPosition = window.pageYOffset;
+  
+      if (scrollPosition > scrollThreshold) {
+        secondSlider.style.display = 'block';
+      }// else {
+    //     secondSlider.style.display = 'none';
+    //   }
+    }
+  
+    window.addEventListener('scroll', handleScroll);
+  });
+  
